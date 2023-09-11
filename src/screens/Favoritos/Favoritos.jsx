d00e171687b9710d95f6f3db81a38a9485c64d25
iskinner21 <iskinner@udesa.edu.ar>
2023-09-11 19:35:08 -0300

    Arranque favoritos

--- src/screens/Favoritos/Favoritos.jsx
+++ src/screens/Favoritos/Favoritos.jsx
@@ -1,11 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import ViewAllTop from "../../components/ViewAllTop/ViewAllTop";
 
-function Favoritos(){
-    return(
-        //Faltaria el componente de buscador//
-        <ViewAllTop/>//Cambiar este componente por el de favoritos
-    )
+class Favoritos extends Component{
+    
 }
 
 
